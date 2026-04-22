24f4c4391e02505edb05fa517c2b8b1a59ceb176
karolwolek <karolwolek06@gmail.com>
2026-04-22 12:03:57 +0200

    fix game printings

--- src/main/scala/com/minimax/BreakThrough.scala
+++ src/main/scala/com/minimax/BreakThrough.scala
@@ -20,12 +20,19 @@ class BreakThrough(
   )
 
   def play() =
+    println("Rozpoczynamy grę w breakthrough!")
     while (!game.isGameOver) {
-      if game.isPlayerOneTurn then game = playerOne.move(game)
-      else game = playerTwo.move(game)
+      if game.isPlayerOneTurn then {
+        println("Ruch gracza pierwszego")
+        game = playerOne.move(game)
+      } else {
+        println("Ruch gracza drugiego")
+        game = playerTwo.move(game)
+      }
+      println(display(game))
     }
-    if (game.playerOneWin) println("Player One win!")
-    else println("Player Two win!")
+    if (game.playerOneWin) println("Grę wygrywa gracz pierwszy")
+    else println("Grę wygrywa gracz drugi")
 
   def display(state: BreakthroughState): String = {
     (1 to boardSize)
